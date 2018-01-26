2479037b723dc7fef706df6fb1a5965ec02f779e
Ömer Akkoyun <33864154+OmerAkkoyun@users.noreply.github.com>
2018-01-26 13:09:27 +0300

    kullanımlar

---
+++ kullanımlari.py
@@ -0,0 +1,27 @@
+bin(4)  #2lik tabanda karşılık gelen kodu verir.
+#çıktısı :0b100
+
+hex(4)#16 lık tabanda karşılık gelen kodu verir(hex olarak)
+#çıktısı :0xf
+ 
+abs(-4) #Mutlak değer diyebiliriz
+#çıktısı :4
+
+round(4.6) #En yakın sayıya yuvarlar.
+#çıktısı : 5
+
+round(4,4) #örnek2
+#çıktısı :4
+
+max(1,2,3,4,5)#enbüyük sayıyı ekrana basar
+#çıktısı : 5
+
+min(1,2,3,4,5) #enküçük sayıya ekrana basar
+#çıktısı :1
+
+sum(1,2,3,4,5) # liste yada demetteki sayıları toplar.
+#çıktısı : 15
+
+pow(5,2) # 1.sayının üssü 2.sayı olur. 5,2 = 5^2 olur
+#çıktısı :25
+ 
